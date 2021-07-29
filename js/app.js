89209d9390748b0075d568e3c76f07755550336c
InsDark <65677103+InsDark@users.noreply.github.com>
2021-07-28 21:10:37 -0500

    new

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-let addItem = document.querySelectorAll('#addItem')
-let itemName = document.querySelectorAll('.item-name')
-
-for (let i = 0; i < addItem.length; i++) {
-    addItem[i].addEventListener('click', () =>{
-        alert(`The ${itemName[i].textContent} was added to the cart`)
-    });
-}
-
-let itemCart = document.querySelector('.items-cart')
-
-itemCart.addEventListener('click', () =>{
-    let itemsStand = document.querySelector('.itemsAdded')
-    itemsStand.classList.toggle('active')
+let itemsCart = document.querySelector('.items-cart')
+let products = document.querySelector('.products').addEventListener('click', e => {
+    addToCart(e);
 })
+
+const addToCart = e => {
+    if (e.target.classList.contains('addItem')){
+        let item = e.target.parentElement;
+        let itemName = item.childNodes[3].childNodes[1].textContent;
+        let itemPrice = item.childNodes[3].childNodes[3].textContent;
+        let newItem = document.createElement("div");
+        newItem.innerHTML = `<h2>${itemName}</h2> <h2>${itemPrice}</h2><i class="fa fa-trash"></i>`
+        itemsCart.appendChild(newItem);
+    }
+    let delItems = document.querySelector('.fa-trash')
+    delItems.addEventListener('click', (e) => {
+        if(e.target.classList.contains('fa-trash')){
+            let itemUp = e.target.parentElement;
+            itemsCart.remove(e.target.parentElement);
+
+        }
+    })
+    e.stopPropagation();
+} 
